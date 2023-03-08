1116ece5ad3f5598d1f021e2d8f8988410ad4ccc
Janos Perge <janos.perge@optimonk.com>
2023-03-08 14:21:05 +0100

    refactor resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,28 +1,7 @@
-const _ = require('lodash');
+const Query = require('./resolvers/query');
+const Session = require('./resolvers/Session');
 
 module.exports = {
-    Query: {
-        sessions: (parent, args, {dataSources}, info) =>{
-            return dataSources.sessionAPI.getSessions(args);
-        },
-        sessionById: (parent, {id}, {dataSources}, info) =>{
-            return dataSources.sessionAPI.getSessionById(id);
-        },
-        speakers: (parent, args, {dataSources}, info)=>{
-            return dataSources.speakerAPI.getSpeakers();
-        },
-        speakersById: (parent, {id}, {dataSources}, info)=>{
-            return dataSources.speakerAPI.getSpeakersById(id);
-        }
-    },
-    Session: {
-        async speakers(session, args, {dataSources}){
-            const speakers = await dataSources.speakerAPI.getSpeakers();
-            const returns = speakers.filter((speaker) => {
-                return _.filter(session.speakers, {id: speaker.id}).length > 0
-            })
-
-            return returns
-        }
-    }
+    Query,
+    Session
 }
---
+++ resolvers/query.js
@@ -0,0 +1,14 @@
+module.exports = {
+    sessions: (parent, args, {dataSources}, info) =>{
+        return dataSources.sessionAPI.getSessions(args);
+    },
+    sessionById: (parent, {id}, {dataSources}, info) =>{
+        return dataSources.sessionAPI.getSessionById(id);
+    },
+    speakers: (parent, args, {dataSources}, info)=>{
+        return dataSources.speakerAPI.getSpeakers();
+    },
+    speakersById: (parent, {id}, {dataSources}, info)=>{
+        return dataSources.speakerAPI.getSpeakersById(id);
+    }
+}
