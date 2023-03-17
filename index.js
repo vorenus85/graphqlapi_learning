9191a0ae42a8e23482091cf95447ea79a58c6a13
Janos Perge <janos.perge@optimonk.com>
2023-03-17 10:07:03 +0100

    add unique error message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {ApolloServer, gql} = require('apollo-server');
+const {ApolloServer, gql, ApolloError} = require('apollo-server');
 const SessionAPI = require('./dataSources/sessions');
 const SpeakerAPI = require('./dataSources/speakers');
 
@@ -11,7 +11,18 @@ const dataSources = () => ({
     speakerAPI: new SpeakerAPI()
 });
 
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    dataSources,
+    debug: false,
+    formatError: (err) =>{
+        if(err.extensions.code == 'INTERNAL_SERVER_ERROR'){
+            return new ApolloError("Wea are having some trouble", "ERROR", {token: "uniqueToken"})
+        }
+        return err;
+    }
+});
 
 server
     .listen({port: process.env.port || 4000})
--- resolvers/session.js
+++ resolvers/session.js
@@ -1,12 +1,19 @@
+const { ApolloError } = require('apollo-server');
 const _ = require('lodash');
 
 module.exports = {
     async speakers(session, args, {dataSources}){
-        const speakers = await dataSources.speakerAPI.getSpeakers();
-        const returns = speakers.filter((speaker) => {
-            return _.filter(session.speakers, {id: speaker.id}).length > 0
-        })
+        try {
+            const speakers = await dataSources.speakerAPI.getSpeakers();
+            const returns = speakers.filter((speaker) => {
+                return _.filter(session.speakers, {id: speaker.id}).length > 0
+            })
 
-        return returns
+            return returns
+
+        } catch (error) {
+            return new ApolloError("Unable to get speakers", "SPEAKERAPIERROR", {token: "UNIQUETOKEN" })
+        }
+        
     }
 }
